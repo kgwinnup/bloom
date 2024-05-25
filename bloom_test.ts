d4854fa839ef4aaed9483d451ac83e90a2e03c65
Cotton Hou <himcotton@gmail.com>
2024-05-25 09:40:45 +0800

    smoking test

--- bloom_test.ts
+++ bloom_test.ts
@@ -1,46 +1,40 @@
 import { Bloom } from "./bloom.ts";
+import { bloom_by, bloom_from } from "./bloom-classless.ts";
 import { assertEquals } from "jsr:@std/assert@^0.204.0";
 
 Deno.test("should create bloom filter with correct properties", () => {
-    const filter = new Bloom(4000, 0.0000001);
+    const filter = bloom_by(4000, 0.0000001);
     assertEquals(filter.k, 23);
     assertEquals(filter.size, 16775);
 });
 
 Deno.test("should insert and lookup in a filter", () => {
-    const filter = new Bloom(4000, 0.0000001);
+    let filter = bloom_by(4000, 0.0000001);
     const input = "hello world";
     const encoder = new TextEncoder();
     const uint8array = encoder.encode(input);
-    filter.insert(uint8array);
+    filter = filter.insert(uint8array);
 
     assertEquals(filter.lookup(uint8array), true);
 });
 
 Deno.test("should only have k non-zero buckets with 1 item inserted", () => {
-    const filter = new Bloom(4000, 0.0000001);
+    const filter = bloom_by(4000, 0.0000001);
     const input = "hello world";
     const encoder = new TextEncoder();
     const uint8array = encoder.encode(input);
-    filter.insert(uint8array);
-
-    // only k buckets should be non-zero
-    let count = 0;
-    for (let i = 0; i < filter.filter.length; i++) {
-        if (filter.filter[i] > 0) {
-            count++;
-        }
-    }
+    const dump = filter.insert(uint8array).dump().subarray(8 + 8);
+    const count = dump.reduce((acc, n) => (n > 0) ? (acc + 1) : acc, 0);
 
     assertEquals(filter.k, count);
 });
 
 Deno.test("should not find random inputs in the filter", () => {
-    const filter = new Bloom(4000, 0.0000001);
+    let filter = bloom_by(4000, 0.0000001);
     const input = "hello world";
     const encoder = new TextEncoder();
     const uint8array = encoder.encode(input);
-    filter.insert(uint8array);
+    filter = filter.batch_insert([ uint8array ]);
     assertEquals(filter.lookup(uint8array), true);
 
     const input2 = "hello world2";
@@ -57,8 +51,6 @@ Deno.test("should insert, dump, read and then look up correctly", () => {
     filter.insert(uint8array);
     assertEquals(filter.lookup(uint8array), true);
 
-    const bytes = filter.dump();
-
-    const filter2 = Bloom.from(bytes);
+    const filter2 = bloom_from(filter.dump());
     assertEquals(filter2.lookup(uint8array), true);
 });
