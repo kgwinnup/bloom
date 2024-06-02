430b171625f841afea6aef2c254dca5526f78182
Kyle Gwinnup <kylegwinnup@gmail.com>
2024-06-02 13:48:21 -0400

    adds failing test

--- bloom_test.ts
+++ bloom_test.ts
@@ -62,3 +62,49 @@ Deno.test("should insert, dump, read and then look up correctly", () => {
     const filter2 = Bloom.from(bytes);
     assertEquals(filter2.lookup(uint8array), true);
 });
+
+Deno.test("should not find random inputs in the filter, sample", () => {
+    function sample(length: number, bytes = 32) {
+        return Array.from({ length }, () => {
+            return crypto.getRandomValues(new Uint8Array(bytes));
+        });
+    }
+
+    const filter = new Bloom(4000, 0.0000001);
+    const input = "hello world";
+    const encoder = new TextEncoder();
+    const uint8array = encoder.encode(input);
+    filter.insert(uint8array);
+    assertEquals(filter.lookup(uint8array), true);
+
+    for (const item of sample(10)) {
+        assertEquals(filter.lookup(item), false, "random in filter");
+    }
+});
+
+Deno.test("should not find random inputs in the filter foo", () => {
+    const n = 4000;
+    const filter = new Bloom(n, 0.0000001);
+    assertEquals(filter.k, 23);
+    assertEquals(filter.size, 16775);
+
+    const inputs = sample(n);
+
+    for (const item of inputs) {
+        filter.insert(item);
+    }
+
+    for (const item of inputs) {
+        assertEquals(filter.lookup(item), true);
+    }
+
+    for (const item of sample(10)) {
+        assertEquals(filter.lookup(item), false, "random in filter");
+    }
+});
+
+function sample(length: number, bytes = 32) {
+    return Array.from({ length }, () => {
+        return crypto.getRandomValues(new Uint8Array(bytes));
+    });
+}
